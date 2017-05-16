b5808add7123acb3905b2fc5891680f90a8601b8
riokosuga <g031n057@s.iwate-pu.ac.jp>
2017-05-16 17:49:09 +0900

    first commit

--- homework5/db_connect.php
+++ homework5/db_connect.php
@@ -2,22 +2,30 @@
 $db_user = 'root';     // ユーザー名
 $db_pass = 'fxtrg25x'; // パスワード
 $db_name = 'bbs';     // データベース名
+
 // MySQLに接続
 $mysqli = new mysqli('localhost', $db_user, $db_pass, $db_name);
 $result = $mysqli->query('select * from `messages`');
 $result_message = '';
+
+//データベース操作
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-  if (!empty($_POST['message'])) {
+  //メッセージの登録
+  if (!empty($_POST['message'] and !empty($_POST['name']))) {
+    $message = $mysqli->real_escape_string($_POST['message']);
     $mysqli->query("insert into `messages` (`name`,`body`) values ('{$_POST['name']}','{$_POST['message']}')");
     $result_message = 'データベースに登録しました！XD';
   } else {
-    $result_message = 'メッセージを入力してください...XO';
+    $result_message = '名前，メッセージを入力してください...XO';
   }
-  if(!empty($POST['del'])){
-    $mysqli->query("insert into 'messages' ('body') values ('{$_POST['message']}')");
-    $result_message = 'メッセージを削除しました;';
+
+  // メッセージの削除
+  if (!empty($_POST['del'])) {
+    $mysqli->query("delete from `messages` where `id` = {$_POST['del']}");
+    $result_message = 'メッセージを削除しました;)';
   }
 }
+
 //データベースからレコード取得
 $result = $mysqli->query('select * from `messages` order by `id` desc');
 ?>
@@ -29,6 +37,7 @@ $result = $mysqli->query('select * from `messages` order by `id` desc');
 
   <body>
     <h1>掲示板</h1>
+    <p><?php echo $result_message; ?></p>
     <h2>書き込みフォーム</h2>
     <form action="" method="post">
       名前：<input type="text" name="name" /><br/>
@@ -43,13 +52,20 @@ $result = $mysqli->query('select * from `messages` order by `id` desc');
           <th>名前</th>
           <th>本文</th>
           <th>投稿時間</th>
+          <th>削除ボタン</th>
         </tr>
         <?php foreach ($result as $row) : ?>
         <tr>
-          <td width="5%" align="center"><?php echo $row['id'] ?></td>
-          <td width="10%" align="center"><?php echo $row['name'] ?></td>
-          <td width="75%"><?php echo $row['body'] ?></td>
-          <td width="10%"><?php echo $row['timestamp'] ?></td>
+          <td width="5%" align="center"><?php echo htmlspecialchars($row['id']) ?></td>
+          <td width="10%" align="center"><?php echo htmlspecialchars($row['name']) ?></td>
+          <td width="70%"><?php echo htmlspecialchars($row['body']) ?></td>
+          <td width="10%"><?php echo htmlspecialchars($row['timestamp']) ?></td>
+          <td width="5%">
+            <form action="" method="post">
+              <input type="hidden" name="del" value="<?php echo $row['id']; ?>" />
+              <input type="submit" value="削除" />
+            </form>
+          </td>
         </tr>
         <?php endforeach; ?>
       </table>
